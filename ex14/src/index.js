8ee48d26c4d9c903ddebe6ba3475632d2983821a
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-18 08:45:52 +0200

    Added a new js file to ex14

---
+++ ex14/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    // Only change code below this line
+    var myNoun = "dog", myVerb = "ran", myAdjective = "big", myAdverb = "quickly";
+    var wordBlanks = "A " + myNoun + " carrying a " + myAdjective + " bone " + myVerb + " home " + myAdverb + ".";
+    // Only change code above this line
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
